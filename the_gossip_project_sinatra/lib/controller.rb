0d1af149a8c9991f27464075ec42fc38359c390f
lucaspiquet <piquet.lucas@gmail.com>
2019-04-23 00:25:41 +0200

    final push : ajouts et vues

--- the_gossip_project_sinatra/lib/controller.rb
+++ the_gossip_project_sinatra/lib/controller.rb
@@ -6,13 +6,20 @@ class ApplicationController < Sinatra::Base
   	erb :index, locals: {gossips: Gossip.all}
 	end
 
+	# url pour créer un gossip
 	get '/gossips/new/' do
     erb :new_gossip
   end
 
-  post '/gossips/new/' do
+  # 2.4.1.: création de l'entrée + sauvegarde dans un fichier gossip.csv
+  post '/gossips/new/' do 
   	Gossip.new(params["gossip_author"], params["gossip_content"]).save
-  	redirect '/'
+  	redirect '/' # 2.4.3. : redirection vers la page d'accueil après sauvegarde de l'entrée
 	end
+
+	# affiche un gossip en particulier, grâce à son id
+  get '/gossips/:id' do
+    erb :show, locals: {id: params['id'].to_i, gossips: Gossip.find(params['id'].to_i)}
+  end
 
 end
--- the_gossip_project_sinatra/lib/gossip.rb
+++ the_gossip_project_sinatra/lib/gossip.rb
@@ -3,6 +3,7 @@ require 'csv'
 class Gossip
 	attr_accessor :author, :content
 
+	# on définit les variables d'instance en les passant en paramètres
   def initialize(author, content)
     @author = author
     @content = content
@@ -14,12 +15,25 @@ class Gossip
   	end
 	end
 
+	# 2.5.1. : chacune des lignes est un objet, que l'on range dans un array (index des gossips)
 	def self.all
   	all_gossips = []
   	CSV.read("./db/gossip.csv").each do |csv_line|
     all_gossips << Gossip.new(csv_line[0], csv_line[1])
-  end
+  	end
   return all_gossips
-end
+	end
+
+	# 2.6.3. : la ligne dans le CSV correspond à l'id, que l'on doit mettre à la fin de l'URL
+	def self.find(id)
+  gossips = []   # permet de stocker la ligne csv demandée
+  CSV.read("./db/gossip.csv").each_with_index do |csv_line, index|
+    if (id == index+1)          # cherche et check si l'index est égale id demandé
+      gossips << Gossip.new(csv_line[0], csv_line[1])    # si trouvé, ajout dans array et break pour retourner l'array
+      break
+        end
+      end
+      return gossips
+  end
 
 end
